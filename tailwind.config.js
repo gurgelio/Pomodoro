4aab2ee61117b061b22538a098106a8234c40762
Leonardo Gurgel <leo@gurgel.io>
2024-07-31 18:59:59 -0300

    optimize fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 // biome-ignore lint/style/noDefaultExport: tailwind expects it to be default
 export default {
 	content: ["./src/**/*.{ts,tsx}"],
 	theme: {
 		fontFamily: {
-			sans: ["Roboto", "sans-serif"],
-			mono: ["RobotoMono", "ui-monospace"],
+			sans: ['"Roboto Flex"', ...defaultTheme.fontFamily.sans],
+			mono: ['"Roboto Mono"', ...defaultTheme.fontFamily.mono],
 		},
-		extend: {},
 	},
 	plugins: [],
 };
